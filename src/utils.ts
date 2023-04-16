737fe38efa59079921c147b1bfc6600f02fee8c0
PyThov <noahthovson@gmail.com>
2023-04-16 15:12:37 -0500

    demo

--- src/utils.ts
+++ src/utils.ts
@@ -28,32 +28,3 @@ export const getPokeData = async (pokemon: string) => {
       return undefined;
     });
 };
-
-export const reorder = (
-  list: IDraggablePokemon[],
-  startIndex: number,
-  endIndex: number
-) => {
-  const result = [...list];
-  const [removed] = result.splice(startIndex, 1);
-  1;
-  result.splice(endIndex, 0, removed);
-
-  return result;
-};
-
-export const remove = (list: IDraggablePokemon[], index: number) => {
-  const result = [...list];
-  result.splice(index, 1);
-  return result;
-};
-
-export const appendAt = (
-  list: IDraggablePokemon[],
-  index: number,
-  pokemon: IDraggablePokemon
-) => {
-  const result = [...list];
-  result.splice(index, 0, pokemon);
-  return result;
-};
